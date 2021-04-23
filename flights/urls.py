ec0e2c224caf43e3e9bc7d90793dfc82716a42c8
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-04-24 04:54:50 +0500

    added response by flight id

--- flights/urls.py
+++ flights/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from flights import views
+import flights
 
 urlpatterns = [
-    path("", views.index, name="index")   
+    path("", views.index, name="index"),   
+    path("<int:flight_id>", views.flight, name="flight")
 ]
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse, HttpResponseNotFound
 import flights
 from django.shortcuts import render
 from .models import Flight
@@ -6,3 +7,11 @@ from .models import Flight
 def index(request):
     flights = Flight.objects.all()
     return render(request, "flights/index.html", {"flights": flights})
+
+def flight(request, flight_id):
+    flights = Flight.objects.all()
+    for flight in flights:
+        if flight_id == flight.id:
+            return HttpResponse(f"Flight {flight.id}: {flight.origin} to {flight.destination}")
+        else:
+            return HttpResponseNotFound(f"No Flight Found!")
