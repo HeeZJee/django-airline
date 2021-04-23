baed7e56260274c25d32c3393b7ac8600a79b80c
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2021-04-24 04:40:06 +0500

    added admin

--- flights/views.py
+++ flights/views.py
@@ -5,6 +5,4 @@ from .models import Flight
 # Create your views here.
 def index(request):
     flights = Flight.objects.all()
-    for flight in flights:
-        print(f"{flight.id}: {flight.origin} to {flight.destination}")
     return render(request, "flights/index.html", {"flights": flights})
